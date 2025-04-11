1e63be4fcb6eb74d55f22eb28ea804e0fce42257
nata20019 <nbernikova68@gmail.com>
2025-04-11 12:30:38 +0200

    mod1044

---
+++ src/cat-api.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_4VE6e6rKxVeaZO3Tf1bitq3cBLIlSr5U1qw82tGCp46uJwWeP5cyD0jngBEyyvgu';
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const select = document.querySelector('.breed-select');
+const error = document.querySelector('.error');
+
+export function fetchCatByBreed(breedId) {
+  return axios(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+    .then(({ data }) => {
+      //   console.log(data);
+      if (data.length === 0) {
+        error.removeAttribute('hidden');
+        return;
+      }
+      return data;
+    })
+    .catch();
+}
+
+export function fetchBreeds() {
+  return axios(`${BASE_URL}/breeds`)
+    .then(({ data }) => {
+      //   console.log(data);
+      if (data.length === 0) {
+        alert(`Пород не знайдено.`);
+      } else {
+        addOptionsToSelect(data);
+      }
+      return data;
+    })
+    .catch(err => {
+      console.log('Помилка під час виконання fetchBreeds:', err);
+      error.removeAttribute('hidden');
+    });
+}
+function addOptionsToSelect(breeds) {
+  select.innerHTML = '';
+  breeds.forEach(breed => {
+    const option = document.createElement('option');
+    option.value = breed.id;
+    option.text = breed.name;
+    select.add(option);
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,65 +1,40 @@
-import axios from 'axios';
-
-axios.defaults.headers.common['x-api-key'] =
-  'live_4VE6e6rKxVeaZO3Tf1bitq3cBLIlSr5U1qw82tGCp46uJwWeP5cyD0jngBEyyvgu';
-import { TheCatAPI } from '@thatapicompany/thecatapi';
+import { fetchCatByBreed, fetchBreeds } from './cat-api';
 
 const select = document.querySelector('.breed-select');
-const list = document.querySelector('.cat-info');
-select.addEventListener('submit', onSelect);
+const catInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+
+select.addEventListener('change', onSelect);
 
 function onSelect(evt) {
   evt.preventDefault();
-  console.log(evt.currentTarget.elements);
-  const { name, description, temperament } = evt.currentTarget.elements;
-  fetchBreeds(name.value, description.value, temperament.value)
-    .then(data => (list.innerHTML = createMarkup(data)))
-    .catch(err => console.log(err));
+  // console.log(evt.target.options);
+  const currentIndexOption = evt.target.selectedIndex;
+  const idCat = evt.target.options[currentIndexOption].value;
+  // console.log(idCat);
+  catInfo.innerHTML = '';
+  loader.removeAttribute('hidden');
+  fetchCatByBreed(idCat).then(data => {
+    if (!data) {
+      return;
+    }
+    // console.log(data[0].breeds[0].description);
+    console.log(data);
+    const { name, description, temperament } = data[0].breeds[0];
+    const { url } = data[0];
+    console.log(name, '---', description, '---', temperament);
+    loader.setAttribute('hidden', true);
+    catInfo.innerHTML = createMarkup(name, description, temperament, url);
+  });
 }
 
-function fetchBreeds(id) {
-  const BASE_URL = 'https://api.thecatapi.com/v1';
-  const API_KEY = 'ce2cb9b2a3da414bb5b172546231704';
-  // const params = new URLSearchParams({
-  //   key: API_KEY,
-  // breed_ids: this.selected_breed.id,
-  // });
+fetchBreeds();
 
-  return fetch(`${BASE_URL}/breeds/search?key=${API_KEY}&name=${id}`)
-    .then(resp => {
-      if (!resp.ok) {
-        throw new Error(`HTTP помилка! статус: ${resp.status}`);
-      }
-      console.log(resp);
-      return resp.json();
-    })
-    .then(data => {
-      console.log(data);
-      if (data.lenght === 0) {
-        console.log(`Породи з іменем "${id}" не знайдено.`);
-        alert(`Породи з іменем "${id}" не знайдено.`);
-      } else {
-        console.log(`Знайдено породи:`, data);
-      }
-      return data;
-    })
-    .catch(err => {
-      console.log('Помилка під час виконання fetchBreeds:', err);
-    });
-}
-fetchBreeds('abys');
-
-function createMarkup(arr) {
-  // return
-  const markup = arr
-    .map(
-      cat => `<li>
-       <img src="${icon}" alt="${text}">
-    <h2>${cat.name}</h2>
-    <p>${cat.description}</p>
-    <p>${cat.temperament}</p>
-</li>`
-    )
-    .join('');
-  // list.innerHTML = markup;
+function createMarkup(name, description, temperament, url) {
+  return `<div>
+       <img src="${url}" alt="" width="300" height="300">
+    <h2>${name}</h2>
+    <p>${description}</p>
+    <p>${temperament}</p>
+</div>`;
 }
